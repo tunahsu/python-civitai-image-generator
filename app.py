1252cf7c15e05e501348a8d852a0d608db87d372
Tuna Hsu <tunahsu45@gmail.com>
2024-07-23 13:08:45 +0800

    Support for users to enter their own API keys

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-import civitai
+import os
+import sys
 import streamlit as st
+import importlib
 from streamlit_extras.concurrency_limiter import concurrency_limiter
 
 
 @concurrency_limiter(max_concurrency=1)
-def generate_image(input_data):
+def generate_image(api_key, input_data):
+    # Reset API key before reload civitai module
+    os.environ['CIVITAI_API_TOKEN'] = api_key
+
+    if 'civitai' in sys.modules:
+        del sys.modules['civitai']
+        civitai = importlib.import_module('civitai')
+    else:
+        import civitai
+
     # Send the request and wait for the response
     try:
         res = civitai.image.create(input_data, wait=True)
@@ -21,8 +32,10 @@ def show_input_data(placeholder, input_data):
 
 
 # Initialization
-st.set_page_config(layout='wide')
-st.title('Civitai Image Generator')
+st.set_page_config(page_title='Civitai Image Generator',
+                   page_icon='🎨',
+                   layout='wide')
+st.title('🎨 :rainbow[Civitai Image Generator]')
 
 model_list = {
     'Realistic-Mix': 'urn:air:sd1:checkpoint:civitai:4384@128713',
@@ -33,35 +46,49 @@ model_list = {
 # Sidebar
 with st.sidebar:
     # Parameters for generated model
-    model = st.selectbox('Model Selection', model_list.keys(), index=0)
+    st.info(
+        'Grab your API Key from your [Civitai account](https://civitai.com/user/account) and paste the token 👇'
+    )
+    api_key = st.text_input(':rainbow[**API Key**]')
+    model = st.selectbox(':rainbow[**Model Selection**]',
+                         model_list.keys(),
+                         index=0)
 
     if model == 'Other':
         model_urn = st.text_input('Model (URN)')
         model_list['Other'] = model_urn
 
-    scheduler = st.selectbox('Scheduler',
+    scheduler = st.selectbox(':rainbow[**Scheduler**]',
                              ['EulerA', 'DPM2MKarras', 'DPMSDEKarras', 'Heun'],
                              index=0)
-    prompt = st.text_area('Prompt (Required)', placeholder='A cat')
-    negative_prompt = st.text_area('Negative Prompt (Optional)',
+    prompt = st.text_area(':rainbow[**Prompt (Required)**]',
+                          placeholder='A cat')
+    negative_prompt = st.text_area(':rainbow[**Negative Prompt (Optional)**]',
                                    placeholder='A dog')
-    steps = st.slider('Steps', min_value=1, max_value=50, value=20)
-    cfg_scale = st.slider('CFG Scale', min_value=1, max_value=15, value=7)
-    width = st.slider('Width', min_value=1, max_value=1024, value=512)
-    height = st.slider('Height', min_value=1, max_value=1024, value=768)
-    seed = st.number_input('Seed (-1 for random seed)', value=-1)
+    steps = st.slider(':rainbow[**Steps**]',
+                      min_value=1,
+                      max_value=50,
+                      value=20)
+    cfg_scale = st.slider(':rainbow[**CFG Scale**]',
+                          min_value=1,
+                          max_value=15,
+                          value=7)
+    width = st.slider(':rainbow[**Width**]',
+                      min_value=1,
+                      max_value=1024,
+                      value=512)
+    height = st.slider(':rainbow[**Height**]',
+                       min_value=1,
+                       max_value=1024,
+                       value=768)
+    seed = st.number_input(':rainbow[**Seed (-1 for random seed)**]', value=-1)
     show = st.checkbox('Show Input Data')
 
 # Main content
 col1, col2 = st.columns(2, gap='large')
 
-with col1:
-    st.header(':gray[Input Data:]')
-with col2:
-    st.header(':gray[Generated Image:]')
-
 if show:
-    if model_list[model] != '' and prompt != '':
+    if api_key != '' and model_list[model] != '' and prompt != '':
         if 'input_data' not in st.session_state:
             st.session_state['input_data'] = {}
 
@@ -82,47 +109,52 @@ if show:
 
         with col1:
             # Show input data (json format)
+            st.subheader(':rainbow[Input Data]')
             placeholder = st.empty()
             show_input_data(placeholder, st.session_state['input_data'])
 
             # Parameters for additional networks
-            additional_networks = st.text_input('Additional Networks (URN)')
-            strength = st.slider('Strength',
-                                 min_value=0.0,
-                                 max_value=1.0,
-                                 value=0.6)
+            with st.expander(':rainbow[**Additional Networks**]',
+                             expanded=False):
+                additional_networks = st.text_input(
+                    ':rainbow[**Model (URN)**]')
+                strength = st.slider('Strength',
+                                     min_value=0.0,
+                                     max_value=1.0,
+                                     value=0.6)
 
-            # If user wants to add additional networks
-            if st.button('Add Additional Networks'):
-                if additional_networks != '':
-                    if 'additionalNetworks' not in st.session_state[
-                            'input_data']:
+                # If user wants to add additional networks
+                if st.button('Add'):
+                    if additional_networks != '':
+                        if 'additionalNetworks' not in st.session_state[
+                                'input_data']:
+                            st.session_state['input_data'][
+                                'additionalNetworks'] = {}
+
                         st.session_state['input_data'][
-                            'additionalNetworks'] = {}
-
-                    st.session_state['input_data'][
-                        'additionalNetworks'].update(
-                            {additional_networks: {
-                                'Strength': strength
-                            }})
-                    show_input_data(placeholder,
-                                    st.session_state['input_data'])
-
-        with col2:
+                            'additionalNetworks'].update(
+                                {additional_networks: {
+                                    'Strength': strength
+                                }})
+                        show_input_data(placeholder,
+                                        st.session_state['input_data'])
             if st.button('Generate'):
                 with st.spinner('Generating...'):
                     # Call the Civitai API and wait for the result
-                    res = generate_image(st.session_state['input_data'])
+                    res = generate_image(api_key,
+                                         st.session_state['input_data'])
 
-                # Show generated image
-                if res:
-                    if 'jobs' in res and res['jobs'][0].get('result'):
-                        # st.write(str(res))
-                        st.image(res['jobs'][0]['result']['blobUrl'],
-                                 caption='Generated Image')
-                        st.toast('Task completed!', icon="✅")
-                    else:
-                        st.error('Failed to retrieve the generated image.')
+                    if res:
+                        # Show generated image
+                        with col2:
+                            if 'jobs' in res and res['jobs'][0].get('result'):
+                                st.subheader(':rainbow[Generated Image]')
+                                st.image(res['jobs'][0]['result']['blobUrl'],
+                                         caption='Generated Image')
+                                st.toast('Task completed!', icon="✅")
+                            else:
+                                st.error(
+                                    'Failed to retrieve the generated image.')
     else:
         st.warning(f'Required fields cannot be empty.')
 else:
